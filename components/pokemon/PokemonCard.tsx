f0d4138678bce62c0fc7db43a97c12636b222016
Yan Vallejo <yavallejor@gmail.com>
2022-04-19 22:35:46 -0500

    Add link details component to pokemonCard

--- components/pokemon/PokemonCard.tsx
+++ components/pokemon/PokemonCard.tsx
@@ -1,4 +1,5 @@
 import { Row, Card, Text, Grid } from "@nextui-org/react";
+import { useRouter } from "next/router";
 import { FC } from "react";
 import { SmallPokemon } from "../../interfaces";
 
@@ -7,9 +8,14 @@ interface Props {
 }
 
 export const PokemonCard: FC<Props> = ({ pokemon }) => {
+    const router = useRouter();
+
+    const onClick = () => {
+        router.push(`/pokemon/${pokemon.id}`);
+    };
     return (
         <Grid xs={6} sm={3} md={2} xl={1} key={pokemon.id}>
-            <Card hoverable clickable>
+            <Card hoverable clickable onClick={onClick}>
                 <Card.Body css={{ p: 1 }}>
                     <Card.Image src={pokemon.img} width="100%" height={140} />
                 </Card.Body>
