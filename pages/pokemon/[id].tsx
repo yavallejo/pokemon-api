aca95c89d2763743966d683890815c7f8e6f0f7e
Yan Vallejo <yavallejor@gmail.com>
2022-04-20 14:43:02 -0500

    feat: :lipstick: Structure pokemon details

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,3 +1,4 @@
+import { Button, Card, Container, Grid, Image, Text } from "@nextui-org/react";
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { pokeApi } from "../../api";
 import { Layout } from "../../components/layouts";
@@ -10,7 +11,70 @@ interface Props {
 const PokemonPage: NextPage<Props> = ({ pokemon }) => {
     return (
         <Layout title="Algun pokemon">
-            <h1>{pokemon.name}</h1>
+            <Grid.Container css={{ marginTop: "5px" }} gap={2}>
+                <Grid xs={12} sm={4}>
+                    <Card hoverable css={{ padding: "30px" }}>
+                        <Card.Body>
+                            <Card.Image
+                                src={
+                                    pokemon.sprites.other?.dream_world
+                                        .front_default || "/no-image.png"
+                                }
+                                alt={pokemon.name}
+                                width="100%"
+                                height={200}
+                            />
+                        </Card.Body>
+                    </Card>
+                </Grid>
+                <Grid xs={12} sm={8}>
+                    <Card css={{ padding: "30px" }}>
+                        <Card.Header
+                            css={{
+                                display: "flex",
+                                justifyContent: "space-between",
+                            }}
+                        >
+                            <Text h1 transform="capitalize">
+                                {pokemon.name}
+                            </Text>
+
+                            <Button color="gradient" ghost>
+                                Guardar en favoritos
+                            </Button>
+                        </Card.Header>
+                        <Card.Body>
+                            <Text size={30}>Sprites</Text>
+                            <Container css={{ display: "flex" }}>
+                                <Image
+                                    src={pokemon.sprites.front_default}
+                                    alt={pokemon.name}
+                                    width={100}
+                                    height={100}
+                                />
+                                <Image
+                                    src={pokemon.sprites.back_default}
+                                    alt={pokemon.name}
+                                    width={100}
+                                    height={100}
+                                />
+                                <Image
+                                    src={pokemon.sprites.front_shiny}
+                                    alt={pokemon.name}
+                                    width={100}
+                                    height={100}
+                                />
+                                <Image
+                                    src={pokemon.sprites.back_shiny}
+                                    alt={pokemon.name}
+                                    width={100}
+                                    height={100}
+                                />
+                            </Container>
+                        </Card.Body>
+                    </Card>
+                </Grid>
+            </Grid.Container>
         </Layout>
     );
 };
