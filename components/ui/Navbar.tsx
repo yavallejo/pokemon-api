9dc88e7a33ee636ec7514ed929229d7410119a1c
Yan Vallejo <yavallejor@gmail.com>
2022-04-19 22:41:25 -0500

    Add link to image logo

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,20 +1,36 @@
-import { useTheme, Text, Spacer } from "@nextui-org/react"
-import Image from 'next/image';
+import { useTheme, Text, Spacer } from "@nextui-org/react";
+import Image from "next/image";
+import Link from "next/link";
 
 export const Navbar = () => {
-  const {theme } = useTheme();
-  return (
-    <div style={{
-      display: 'flex',
-      alignItems: 'center',
-      padding: '10px 18px',
-      backgroundColor:theme?.colors.gray900.value
-    }}>
-    <Image src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" alt="Icono del pokemon" width={70} height={70}/>
-    <Text color="white" h2>P</Text>
-    <Text color="white" h3>okemon</Text>
-    <Spacer css={{flex:1}}/>
-    <Text color="white" h3>Favoritos</Text>
-    </div>
-  )
-}
+    const { theme } = useTheme();
+    return (
+        <Link href="/">
+            <a
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    padding: "10px 18px",
+                    backgroundColor: theme?.colors.gray900.value,
+                }}
+            >
+                <Image
+                    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+                    alt="Icono del pokemon"
+                    width={70}
+                    height={70}
+                />
+                <Text color="white" h2>
+                    P
+                </Text>
+                <Text color="white" h3>
+                    okemon
+                </Text>
+                <Spacer css={{ flex: 1 }} />
+                <Text color="white" h3>
+                    Favoritos
+                </Text>
+            </a>
+        </Link>
+    );
+};
