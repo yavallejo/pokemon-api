183f72587938b211258b3a162c2b699ee7d753f4
Yan Vallejo <yavallejor@gmail.com>
2022-04-25 14:17:50 -0500

    feat: :sparkles: Add route by name pokemon

---
+++ pages/name/[name].tsx
@@ -0,0 +1,139 @@
+import { useState } from "react";
+
+import { Button, Card, Container, Grid, Image, Text } from "@nextui-org/react";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import confetti from "canvas-confetti";
+
+import { pokeApi } from "../../api";
+import { Layout } from "../../components/layouts";
+import { Pokemon, PokemonListResponse } from "../../interfaces";
+import { localFavorites } from "../../utils";
+
+interface Props {
+    pokemon: Pokemon;
+}
+
+const PokemonByNamePage: NextPage<Props> = ({ pokemon }) => {
+    const [isInFavorites, setIsInFavorites] = useState(
+        localFavorites.existInFavorites(pokemon.id)
+    );
+
+    const onToggleFavorites = () => {
+        localFavorites.toggleFavorites(pokemon.id);
+        setIsInFavorites(!isInFavorites);
+        if (isInFavorites) return;
+
+        confetti({
+            zIndex: 999,
+            particleCount: 100,
+            spread: 160,
+            angle: -100,
+            origin: {
+                x: 1,
+                y: 0,
+            },
+        });
+    };
+
+    return (
+        <Layout title="Algun pokemon">
+            <Grid.Container css={{ marginTop: "5px" }} gap={2}>
+                <Grid xs={12} sm={4}>
+                    <Card hoverable css={{ padding: "30px" }}>
+                        <Card.Body>
+                            <Card.Image
+                                src={
+                                    pokemon.sprites.other?.dream_world
+                                        .front_default || "/no-image.png"
+                                }
+                                alt={pokemon.name}
+                                width="100%"
+                                height={200}
+                            />
+                        </Card.Body>
+                    </Card>
+                </Grid>
+                <Grid xs={12} sm={8}>
+                    <Card css={{ padding: "30px" }}>
+                        <Card.Header
+                            css={{
+                                display: "flex",
+                                flexDirection: "column",
+                                justifyContent: "space-between",
+                            }}
+                        >
+                            <Text h1 transform="capitalize">
+                                {pokemon.name}
+                            </Text>
+
+                            <Button
+                                color="gradient"
+                                ghost={!isInFavorites}
+                                onClick={onToggleFavorites}
+                            >
+                                {isInFavorites
+                                    ? "En Favoritos"
+                                    : "Guardar en favoritos"}
+                            </Button>
+                        </Card.Header>
+                        <Card.Body>
+                            <Text size={30}>Sprites</Text>
+                            <Container css={{ display: "flex" }}>
+                                <Image
+                                    src={pokemon.sprites.front_default}
+                                    alt={pokemon.name}
+                                    width={100}
+                                    height={100}
+                                />
+                                <Image
+                                    src={pokemon.sprites.back_default}
+                                    alt={pokemon.name}
+                                    width={100}
+                                    height={100}
+                                />
+                                <Image
+                                    src={pokemon.sprites.front_shiny}
+                                    alt={pokemon.name}
+                                    width={100}
+                                    height={100}
+                                />
+                                <Image
+                                    src={pokemon.sprites.back_shiny}
+                                    alt={pokemon.name}
+                                    width={100}
+                                    height={100}
+                                />
+                            </Container>
+                        </Card.Body>
+                    </Card>
+                </Grid>
+            </Grid.Container>
+        </Layout>
+    );
+};
+
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+    const { data } = await pokeApi.get<PokemonListResponse>(
+        "/pokemon?limit=151"
+    );
+
+    const pokemonName: string[] = data.results.map((pokemon) => pokemon.name);
+    return {
+        paths: pokemonName.map((name) => ({
+            params: { name },
+        })),
+        fallback: false,
+    };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const { name } = params as { name: string };
+    const { data } = await pokeApi.get<Pokemon>(`pokemon/${name}`); // your fetch function here
+    return {
+        props: {
+            pokemon: data,
+        },
+    };
+};
+
+export default PokemonByNamePage;
