12b86f9c07d0a53730e80dee4baf6154259e3872
Yan Vallejo <yavallejor@gmail.com>
2022-04-25 21:43:43 -0500

    feat: :sparkles: Add open Graph

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -8,6 +8,8 @@ interface Props {
     children?: React.ReactNode;
 }
 
+const origin = typeof window === "undefined" ? "" : window.location.origin;
+
 export const Layout: FC<Props> = ({ children, title }) => {
     return (
         <>
@@ -19,6 +21,19 @@ export const Layout: FC<Props> = ({ children, title }) => {
                     content={`Información sobre el pokémon ${title}`}
                 />
                 <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+
+                <meta
+                    property="og:title"
+                    content={`Información sobre ${title}`}
+                />
+                <meta
+                    property="og:description"
+                    content={`esta es la pagina sobre el pokémon ${title}`}
+                />
+                <meta
+                    property="og:image"
+                    content={`${origin}/img/banner.png`}
+                />
             </Head>
 
             <Navbar />
