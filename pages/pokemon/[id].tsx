16e8738f010778a0246daef7f3febab1a07aa31b
Yan Vallejo <yavallejor@gmail.com>
2022-05-03 21:07:03 -0500

    feat: :sparkles: Add incremental static generation

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -113,22 +113,35 @@ const PokemonPage: NextPage<Props> = ({ pokemon }) => {
 
 export const getStaticPaths: GetStaticPaths = async (ctx) => {
     const pokemons151 = [...Array(151)].map((value, index) => `${index + 1}`);
+
     return {
         paths: pokemons151.map((id) => ({
             params: { id },
         })),
-        fallback: false,
+        // fallback: false
+        fallback: "blocking",
     };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
     const { id } = params as { id: string };
 
+    const pokemon = await getPokemon(id);
+
+    if (!pokemon) {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        };
+    }
+
     return {
         props: {
-            pokemon: await getPokemon(id),
+            pokemon,
         },
-        revalidate: 86400,
+        revalidate: 86400, // 60 * 60 * 24,
     };
 };
 
--- utils/getPokemon.ts
+++ utils/getPokemon.ts
@@ -2,11 +2,14 @@ import { pokeApi } from "../api";
 import { Pokemon } from "../interfaces";
 
 export const getPokemon = async (nameOrId: string) => {
-    const { data } = await pokeApi.get<Pokemon>(`pokemon/${nameOrId}`); // your fetch function here
-
-    return {
-        id: data.id,
-        name: data.name,
-        sprites: data.sprites,
-    };
+    try {
+        const { data } = await pokeApi.get<Pokemon>(`pokemon/${nameOrId}`); // your fetch function here
+        return {
+            id: data.id,
+            name: data.name,
+            sprites: data.sprites,
+        };
+    } catch (error) {
+        return null;
+    }
 };
